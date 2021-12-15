3600711ae432ac48171599aa52980efae6f42c99
spbacilio <39282515+spbacilio@users.noreply.github.com>
2021-12-15 10:32:47 -0600

    Agrego el primer modulo Pre Entrada

---
+++ src/app/website/pre-entrada/pre-entrada.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PreEntradaRoutingModule } from './pre-entrada-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PreEntradaRoutingModule
+  ]
+})
+export class PreEntradaModule { }
